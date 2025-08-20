885212ae673a2d4392101c22b0029b8608d41180
Lingfeng Mao <Lingfeng.Mao@synaptics.com>
2025-08-20 11:23:18 +0000

    Revert "drivers: mmc: sdhi-of-dwcmshc: move dll calibration before tuning"
    This reverts commit 66d6119a09c5176f3c4c44aa8c3efd1d347f9641.
    
    Change-Id: Id86ff35f744e9122e76fc9d715e9fcadba6e91af
    Reviewed-on: https://gerrit-sha.synaptics.com/gerrit/272429
    Reviewed-by: Lingfeng Mao <Lingfeng.Mao@synaptics.com>

--- drivers/mmc/host/sdhci-of-dwcmshc.c
+++ drivers/mmc/host/sdhci-of-dwcmshc.c
@@ -135,7 +135,6 @@ struct dwcmshc_priv {
 	u8			pad_sn;
 	u8			pad_sp;
 	u8			drv_strength;
-	u8			txdelay;
 	bool			dll_cal;
 	bool			mode1_tune;
 	u32			dll_delay_offset;
@@ -721,7 +720,7 @@ static void dwcmshc_set_uhs_signaling(struct sdhci_host *host,
 	else if (timing == MMC_TIMING_MMC_HS200)
 		txdelay = 40;
 	else if (timing == MMC_TIMING_MMC_HS400)
-		txdelay = priv->txdelay;
+		txdelay = dwcmshc_choose_hs400_txdelay(host, priv);
 
 	if (txdelay)
 		dwcmshc_set_phy_tx_delay(host, txdelay);
@@ -735,12 +734,6 @@ static int dwcmshc_execute_tuning(struct mmc_host *mmc, u32 opcode)
 	u32 vendor_ptr = priv->vendor_ptr;
 	u16 clk;
 	u32 val;
-
-	if (opcode == MMC_SEND_TUNING_BLOCK_HS200) {
-		priv->txdelay = dwcmshc_choose_hs400_txdelay(host, priv);
-		if (priv->txdelay)
-			dwcmshc_set_phy_tx_delay(host, priv->txdelay);
-	}
 
 	if (priv->mode1_tune) {
 		if (host->tuning_mode != SDHCI_TUNING_MODE_1)
